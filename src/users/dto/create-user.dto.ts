852254981ca261b427ecec18400b28244a258af9
ferdudu530 <xdd530@icloud.com>
2023-06-06 19:34:14 -0700

    added decorators to columns of the table

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,19 @@
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty, ArrayUnique } from 'class-validator';
 
 export class CreateUserDto {
+    @IsNotEmpty()
     readonly id: number;
+    @IsNotEmpty()
     @IsString()
     readonly name: string;
+    @IsNotEmpty()
+    @ArrayUnique()
     @IsString()
     readonly email: string;
+    @IsNotEmpty()
     @IsString()
     readonly password: string;
+    @IsNotEmpty()
     @IsString()
     readonly role: string;
 }

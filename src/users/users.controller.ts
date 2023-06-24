ca04317d910c4b5ad46bc1fa92eacaa286e92e3d
ferdudu530 <xdd530@icloud.com>
2023-06-23 19:42:03 -0700

    save for experiment

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,18 @@
-import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, SetMetadata, } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { REQUEST } from '@nestjs/core';
 import { UsersService } from './users.service';
+import { Roles } from 'src/roles.decorator';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly UsersService: UsersService, @Inject(REQUEST) private request: Request) {}
 
-    @Post()
-    create(@Body() CreateUserDto: CreateUserDto) {
-        return this.UsersService.create(CreateUserDto);
-    }
+    // @Post()
+    // create(@Body() CreateUserDto: CreateUserDto) {
+    //     return this.UsersService.create(CreateUserDto);
+    // }
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.UsersService.remove(id);
@@ -28,5 +29,11 @@ export class UsersController {
     findAll() {
         // const { limit, offset } = paginationQuery;
         return this.UsersService.findAll();
+    }
+
+    @Post()
+    @Roles('admin')
+    async create(@Body() CreateCatDto: CreateUserDto) {
+        return this.UsersService.create(CreateCatDto);
     }
 }
